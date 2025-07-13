a9c5026f02dfe962c151c5561b8cc1f40edf3108
Robertngatia25 <ngatiah.robert@gmail.com>
2025-07-14 01:01:30 +0300

    Add the addmin,.py config

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,3 +1,27 @@
-from django.contrib import admin
+# bookshelf/admin.py
 
-# Register your models here.
+from django.contrib import admin
+from .models import Book
+
+# ----------------------------------------------------
+# Define a custom ModelAdmin for the Book model
+# ----------------------------------------------------
+
+class BookAdmin(admin.ModelAdmin):
+    # 1. Customize the list view display (fields shown in the table)
+    list_display = ('title', 'author', 'publication_year')
+
+    # 2. Add filters to the sidebar
+    list_filter = ('publication_year', 'author')
+
+    # 3. Add search capabilities (searches the specified fields)
+    search_fields = ('title', 'author')
+
+    # Optional: Order the list view by publication year descending
+    ordering = ('-publication_year',)
+
+# ----------------------------------------------------
+# Register the Book model with the custom admin class
+# ----------------------------------------------------
+
+admin.site.register(Book, BookAdmin)
