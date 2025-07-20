e941a0c256f26ce6164e020d418f82f7a6831e2c
Robertngatia25 <ngatiah.robert@gmail.com>
2025-07-21 00:02:23 +0300

    ADD THE LOGOUT

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
    path('books/', list_books, name='list_books'),
    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
    path('login/', auth_views.LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
-   path('logout/', auth_views.LogoutView.as_view(next_page='relationship_app:login'), name='logout'),
+   path('logout/', auth_views.LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
    path('register/', views.register, name='register'),
    path('admin-view/', views.admin_view, name='admin_view'),
    path('librarian-view/', views.librarian_view, name='librarian_view'),
