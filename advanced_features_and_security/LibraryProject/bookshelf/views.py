6d1025ed7f06db127a53f016e483c36eb01b6136
Robertngatia25 <ngatiah.robert@gmail.com>
2025-07-26 18:49:06 +0300

    Add all the correction

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import permission_required
 from .models import Book
 from django.http import HttpResponse
+from .forms import BookForm
 
 def search_books(request):
     query = request.GET.get("q")
@@ -36,3 +37,30 @@ def secure_view(request):
     response = HttpResponse("Secure Content")
     response["Content-Security-Policy"] = "default-src 'self'"
     return response
+@permission_required('relationship_app.can_add_book', raise_exception=True)
+def add_book(request):
+    if request.method == 'POST':
+        form = BookForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('book-list')
+    else:
+        form = BookForm()
+    return render(request, 'relationship_app/book_form.html', {'form': form})
+
+@permission_required('relationship_app.can_change_book', raise_exception=True)
+def edit_book(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    form = BookForm(request.POST or None, instance=book)
+    if form.is_valid():
+        form.save()
+        return redirect('book-list')
+    return render(request, 'relationship_app/book_form.html', {'form': form})
+
+@permission_required('relationship_app.can_delete_book', raise_exception=True)
+def delete_book(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    if request.method == 'POST':
+        book.delete()
+        return redirect('book-list')
+    return render(request, 'relationship_app/book_confirm_delete.html', {'book': book})
--- advanced_features_and_security/LibraryProject/relationship_app/views.py
+++ advanced_features_and_security/LibraryProject/relationship_app/views.py
@@ -8,7 +8,6 @@ from django.contrib.auth import login, logout, aauthenticate
 from django.contrib.auth.decorators import login_required, user_passes_test
 from django.contrib.auth.decorators import permission_required
 from .models import UserProfile
-from .forms import Bookform
 
 
 
@@ -80,31 +79,3 @@ def librarian_view(request):
 @user_passes_test(is_member)
 def member_view(request):
     return render(request, 'relationship_app/member_view.html')
-
-@permission_required('relationship_app.can_add_book', raise_exception=True)
-def add_book(request):
-    if request.method == 'POST':
-        form = BookForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('book-list')
-    else:
-        form = BookForm()
-    return render(request, 'relationship_app/book_form.html', {'form': form})
-
-@permission_required('relationship_app.can_change_book', raise_exception=True)
-def edit_book(request, pk):
-    book = get_object_or_404(Book, pk=pk)
-    form = BookForm(request.POST or None, instance=book)
-    if form.is_valid():
-        form.save()
-        return redirect('book-list')
-    return render(request, 'relationship_app/book_form.html', {'form': form})
-
-@permission_required('relationship_app.can_delete_book', raise_exception=True)
-def delete_book(request, pk):
-    book = get_object_or_404(Book, pk=pk)
-    if request.method == 'POST':
-        book.delete()
-        return redirect('book-list')
-    return render(request, 'relationship_app/book_confirm_delete.html', {'book': book})
