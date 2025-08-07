0a4b3d9c5076f23a571ab78619a971f1833b053b
Robertngatia25 <ngatiah.robert@gmail.com>
2025-08-07 22:17:28 +0300

    Add the finalized advanced

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,3 +1,42 @@
 from django.shortcuts import render
-
+from rest_framework import generics, permissions
+from .models import Book
+from .serializers import BookSerializer
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 # Create your views here.
+
+# List all books
+class BookListView(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.AllowAny]  # Publicly accessible
+
+# Retrieve a specific book
+class BookDetailView(generics.RetrieveAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.AllowAny]  # Publicly accessible
+    
+
+# Create a new book
+class BookCreateView(generics.CreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]  # Authenticated to create
+    def perform_create(self, serializer):
+        # e.g., log or associate with user
+        serializer.save()
+
+# Update a book
+class BookUpdateView(generics.UpdateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    def perform_update(self, serializer):
+        serializer.save()
+
+# Delete a book
+class BookDeleteView(generics.DestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
