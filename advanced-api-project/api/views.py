a9aa493ea9d0740258872c0d38304678f47c3ae2
Robertngatia25 <ngatiah.robert@gmail.com>
2025-08-09 23:37:51 +0300

    The authentication token

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -2,18 +2,19 @@ from django.shortcuts import render
 from rest_framework import generics, permissions
 from .models import Book
 from .serializers import BookSerializer
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 
 # List all books (read-only for unauthenticated users)
 class BookListView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = [permissions.AllowAny]
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
 # Retrieve one book by ID (read-only for unauthenticated users)
 class BookDetailView(generics.RetrieveAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = [permissions.AllowAny]
+    permission_classes = [IsAuthenticated]
 
 # Create a new book (only authenticated users)
 class BookCreateView(generics.CreateAPIView):
