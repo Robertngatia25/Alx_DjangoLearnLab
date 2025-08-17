ac5eeb20c1ff9fc0c959dfa6da404db664111a0c
Robertngatia25 <ngatiah.robert@gmail.com>
2025-08-17 23:37:47 +0300

    Add the url path

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -21,7 +21,7 @@ urlpatterns = [
     path("register/", register_view, name="register"),
     path("profile/", profile_view, name="profile"),
 
-    path('posts/<int:post_id>/comments/new/', CommentCreateView.as_view(), name='comment-create'),
-    path('comments/<int:pk>/update/', CommentUpdateView.as_view(), name='comment-update'),
-    path('comments/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment-delete'),
+    path("post/<int:pk>/comments/new/", CommentCreateView.as_view(), name="comment-create"),
+    path("comment/<int:pk>/update/", CommentUpdateView.as_view(), name="comment-update"),
+    path("comment/<int:pk>/delete/", CommentDeleteView.as_view(), name="comment-delete"),
 ]
