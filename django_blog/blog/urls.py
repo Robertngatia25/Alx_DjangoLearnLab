a1d5dbc80a41f59af4e0ee95f86b50f7413c53b3
Robertngatia25 <ngatiah.robert@gmail.com>
2025-08-17 23:24:17 +0300

    Add the url paths correctly

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -11,8 +11,8 @@ urlpatterns = [
     path("posts/", PostListView.as_view(), name="posts"),              # list
     path("posts/new/", PostCreateView.as_view(), name="post-create"),  # create
     path("posts/<int:pk>/", PostDetailView.as_view(), name="post-detail"),  # detail
-    path("posts/<int:pk>/edit/", PostUpdateView.as_view(), name="post-edit"),  # update
-    path("posts/<int:pk>/delete/", PostDeleteView.as_view(), name="post-delete"), 
+    path("post/<int:pk>/update/", PostUpdateView.as_view(), name="post-update"),  # update
+    path("post/<int:pk>/delete/", PostDeleteView.as_view(), name="post-delete"),
 
     # auth
     path("login/", UserLoginView.as_view(), name="login"),
