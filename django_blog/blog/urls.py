ff3a6f9f95fc587abfeea059c40957b7ccb47f0a
Robertngatia25 <ngatiah.robert@gmail.com>
2025-08-17 23:21:32 +0300

    The crud url paths added

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -8,12 +8,12 @@ urlpatterns = [
     path("", home_view, name="home"),
 
   # CRUD routes
-    path("posts/", PostListView.as_view(), name="posts"),
-    path("posts/new/", PostCreateView.as_view(), name="post-create"),
-    path("posts/<int:pk>/", PostDetailView.as_view(), name="post-detail"),
-    path("posts/<int:pk>/edit/", PostUpdateView.as_view(), name="post-edit"),
-    path("posts/<int:pk>/delete/", PostDeleteView.as_view(), name="post-delete"),
-    
+    path("posts/", PostListView.as_view(), name="posts"),              # list
+    path("posts/new/", PostCreateView.as_view(), name="post-create"),  # create
+    path("posts/<int:pk>/", PostDetailView.as_view(), name="post-detail"),  # detail
+    path("posts/<int:pk>/edit/", PostUpdateView.as_view(), name="post-edit"),  # update
+    path("posts/<int:pk>/delete/", PostDeleteView.as_view(), name="post-delete"), 
+
     # auth
     path("login/", UserLoginView.as_view(), name="login"),
     path("logout/", UserLogoutView.as_view(), name="logout"),
